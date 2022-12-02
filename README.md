d65bba082774b7538e6c2f4ebec59c9ca43a9d78
wahmed <waqar.ahmed@iit.it>
2022-12-02 20:24:25 +0100

    changes to start training

--- README.md
+++ README.md
@@ -46,12 +46,4 @@ in a terminal to start training.
 The included "config.txt" assumes that the dataset "sn2_reactions.npz" is present. It can be downloaded from: https://zenodo.org/record/2605341. In order to use a different dataset, it needs to be formatted in the same way as this example ("sn2_reactions.npz"). Please refer to the README file of the dataset (available from https://zenodo.org/record/2605341) for details.
 
 
-## How to cite
-
-If you find this software useful, please cite:
-
-```
-Unke, O. T. and Meuwly, M. "PhysNet: A Neural Network for Predicting Energies, Forces, Dipole Moments and Partial Charges" arxiv:1902.08408 (2019).
-```
-
 
--- data/read_data.py
+++ data/read_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # read dataset into memory
-data = np.load("sn2_reactions.npz")
+data = np.load("./data/sn2_reactions.npz")
 
 # print contents to console
 print("Contents:")
@@ -11,7 +11,7 @@ print()
 
 # as example for accessing individual entries, print the data for entry idx=0
 idx = 0
-print("Data for entry " + str(idx)+":")
+print("Data for entry " + str(idx) + ":")
 print("Number of atoms")
 print(data["N"][idx])
 print("Energy [eV]")
@@ -19,10 +19,10 @@ print(data["E"][idx])
 print("Total charge")
 print(data["Q"][idx])
 print("Dipole moment vector (with respect to [0.0 0.0 0.0]) [eA]")
-print(data["D"][idx,:])
+print(data["D"][idx, :])
 print("Nuclear charges")
-print(data["Z"][idx,:data["N"][idx]])
+print(data["Z"][idx, : data["N"][idx]])
 print("Cartesian coordinates [A]")
-print(data["R"][idx,:data["N"][idx],:])
+print(data["R"][idx, : data["N"][idx], :])
 print("Forces [eV/A]")
-print(data["F"][idx,:data["N"][idx],:])
+print(data["F"][idx, : data["N"][idx], :])
